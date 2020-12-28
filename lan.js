c00b3aaed74198eef1da681afe380870fd0761a5
Hinako-Tanaka <youngbird1228@gmail.com>
2020-12-28 23:17:16 +0900

    set foreign languages

---
+++ lan.js
@@ -0,0 +1,80 @@
+glot.assign('top-name', {
+    ja: '辛くない韓国食堂',
+    en: 'No Spicy Korean Restaurant',
+    ko: '안 매운 한국식당'
+})
+glot.assign('gnav-item-menu', {
+    ja: 'メニュー',
+    en: 'MENU',
+    ko: '안 매운 한국식당'
+})
+glot.assign('gnav-item-intro', {
+    ja: 'お店の紹介',
+    en: 'INTRODUCTION',
+    ko: '가게 소개'
+})
+glot.assign('gnav-item-info', {
+    ja: 'お知らせ',
+    en: 'INFORMATION',
+    ko: '공지'
+})
+glot.assign('header-heading', {
+    ja: 'アカイけど、大丈夫',
+    en: 'No Spicy Korean Restaurant',
+    ko: '안 매운 한국식당'
+})
+glot.assign('info-txt1', {
+    ja: '辛い食べ物があまり得意ではない、しかし韓国料理を食べて韓国の食文化を感じたいという想いで開店しました。',
+    en: 'Do you like so spicy food? If your answer is No, we recommend <No Spicy Korean Restaurant>. You can feel Korean food culture without tasting hot flaver.',
+    ko: '매운 음식을 좋아하지 않으시는 분! 하지만 한식을 먹고 한식문화를 즐기고 싶은 분! 당신의 소원을 들어드릴 마음으로 오픈했습니다.'
+})
+glot.assign('info-txt2', {
+    ja: '写真に見える赤色は唐辛子の赤ではなく、実は赤パプリカの赤なんです。',
+    en: 'The red color in pictures are not the color of peppers, but of red paprica.',
+    ko: '사진 안에 보이시는 빨간 색은 고추 색깔 아니고 사실 빨간 파프리카입니다!'
+})
+glot.assign('menu1', {
+    ja: 'ビビンバの花咲く頃',
+    en: 'When the Bibimbap Blooms',
+    ko: '비빔밥 꽃 필 무렵'
+})
+glot.assign('menu1-txt', {
+    ja: '赤・オレンジ・黄・黄緑・緑・茶・白の色鮮やかな花を咲かせます。',
+    en: 'There are flowers that have many brilliant colors.',
+    ko: '빨간 꽃, 주황 꽃, 노란 꽃, 초록 꽃, 갈 꽃, 하얀 꽃이 핀 비빔밥 그릇.'
+})
+glot.assign('menu2', {
+    ja: 'キンパはいったい、なぜ？',
+    en: 'Whats Worong With Kimbap',
+    ko: '김밥이 왜 그럴까'
+})
+glot.assign('menu2-txt', {
+    ja: 'キンパが愛される理由とは、手軽に、一口で、ご飯・ナムル・お肉・海苔を摂取できることにあります。',
+    en: 'The reason why people like Kimbap is its easyness to eat and high nutritional value.',
+    ko: '많은 사람들이 김밥을 좋아하는 이유는 바로 탄수화물, 식물섬유, 단백질을 삽취할 수 있다는 점일 것입니다.,'
+})
+glot.assign('menu3', {
+    ja: 'マンドゥの声が聞こえる',
+    en: 'I Can Hear Mandu Voice',
+    ko: '만두의 목소리가 들려'
+})
+glot.assign('menu3-txt', {
+    ja: '揚げマンドゥを口に入れた瞬間の"カリッ"という声、聴いてみたいと思いませんか？',
+    en: 'When the Bibimbap Blooms',
+    ko: '만두를 먹을 떄 바삭 바삭 소리를 들어보고 싶으세요?'
+})
+// glot.assign('menu1', {
+//     ja: 'ビビンバの花咲く頃',
+//     en: 'When the Bibimbap Blooms',
+//     ko: '비빔밥 꽃 필 무렵'
+// })
+// glot.assign('menu1', {
+//     ja: 'ビビンバの花咲く頃',
+//     en: 'When the Bibimbap Blooms',
+//     ko: '비빔밥 꽃 필 무렵'
+// })
+// glot.assign('menu1', {
+//     ja: 'ビビンバの花咲く頃',
+//     en: 'When the Bibimbap Blooms',
+//     ko: '비빔밥 꽃 필 무렵'
+// })
