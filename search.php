25206f524ba7db44f366388a0aec5486a4f4c073
Hinako-Tanaka <youngbird1228@gmail.com>
2021-02-03 23:07:05 +0900

    initial commit

---
+++ search.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>検索ページ</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <p>検索したい日付を入力してください。</p>
+        <input type="date" name="date">
+        <input type="submit" value="検索">
+    </form>
+
+    <?php
+    // 1. DBに接続
+    $dsn = 'mysql:dbname=koreanrestaurant;host=localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
+
+    // POSTでデータが送信されたときのみSQLを実行する
+    if (!empty($_POST)) {
+        // 2. SQL文の実行
+        // データ（bookingテーブルのdateカラムのデータ）を読み込む
+        $sql = 'SELECT * FROM `booking` WHERE `date` = ?';
+        $data[] = $_POST['date'];
+        $stmt = $dbh->prepare($sql);
+        // var_dump($sql);
+        // die;
+        $stmt->execute($data);
+
+
+        // データを取得
+        while(1){
+            // レコードを一件ずつ取り出し、それを連想は入れる（カラム名がキーになる）に変換する
+            $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($rec == false) {
+                break;
+            }
+            echo 'お名前：' . $rec['name'] . '<br>';
+            echo '来店日時：' . $rec['date'] . '<br>';
+            echo '来店時間：' . $rec['time'] . '<br>';
+            echo '人数：' . $rec['people'] . '<br>';
+            echo 'メアド：' . $rec['email'] . '<br>';
+            echo 'ご要望：' . $rec['request'] . '<br>';
+            echo '<hr>';
+
+        }
+    }
+
+    // 3. DBの切断
+    $dbh = null;
+    ?>
+</body>
+
+</html>
---
+++ search2.php
@@ -0,0 +1,50 @@
+<?php
+  // １．データベースに接続する
+  $dsn = 'mysql:dbname=koreanrestaurant;host=localhost';
+  $user = 'root';
+  $password = '';
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8');
+
+  // POSTでデータが送信された時のみSQLを実行する
+  if (!empty($_POST)) {
+    // ２．SQL文を実行する
+    $sql = 'SELECT * FROM `booking` WHERE `date` = ' . $_POST['date'];
+
+    $stmt = $dbh->prepare($sql);
+    // var_dump($sql);
+    // die;
+    $stmt->execute();
+
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    // var_dump($rec);
+    // die;
+
+    // SQLを実行
+    // $stmt = $dbh->prepare($sql);
+    // $stmt->execute();
+
+  }
+
+  // ３．データベースを切断する
+  $dbh = null;
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>検索ページ</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <form action="" method="post">
+    <p>検索したいcodeを入力してください。</p>
+    <input type="date" name="date">
+    <input type="submit" value="検索">
+  </form>
+  <?php foreach ($stmt as $row):?>
+    <?php echo $row[0]; ?>
+  <?php endforeach; ?>
+  
+</body>
+</html>
