96e4043ca1890d7ea2d566c0999f173318cc0151
Hinako-Tanaka <youngbird1228@gmail.com>
2021-02-03 18:14:59 +0900

    initial commit

---
+++ view.php
@@ -0,0 +1,32 @@
+<?php 
+// 1. DBに接続
+$dsn = 'mysql:dbname=koreanrestaurant;host=localhost'; 
+$user = 'root'; 
+$password=''; 
+$dbh = new PDO($dsn, $user, $password); 
+$dbh->query('SET NAMES utf8'); 
+
+// 2. SQL文を実行
+$sql = 'SELECT * FROM `booking`'; 
+$stmt = $dbh->prepare($sql); 
+$stmt->execute(); 
+
+while(1) {
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($rec == false) {
+        break;
+    }
+
+    echo $rec['name'] . '<br>';
+    echo $rec['date'] . '<br>';
+    echo $rec['time'] . '<br>';
+    echo $rec['people'] . '<br>';
+    echo $rec['email'] . '<br>';
+    echo $rec['request'] . '<br>';
+    echo '<hr>';
+
+}
+
+// 3. DBを切断
+$dbh = null; 
+?>
