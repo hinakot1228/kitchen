4ccb92672d116369ee7130bb790d12edf32c9757
Hinako-Tanaka <youngbird1228@gmail.com>
2021-02-07 19:15:56 +0900

    third commit

--- search.php
+++ search.php
@@ -16,11 +16,17 @@
 
     <?php
     // 1. DBに接続
-    $dsn = 'mysql:dbname=koreanrestaurant;host=localhost';
-    $user = 'root';
-    $password = '';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->query('SET NAMES utf8');
+    // $dsn = 'mysql:dbname=koreanrestaurant;host=localhost';
+    // $user = 'root';
+    // $password = '';
+    // $dbh = new PDO($dsn, $user, $password);
+    // $dbh->query('SET NAMES utf8');
+
+    $host = getenv('us-cdbr-east-03.cleardb.com'); //MySQLがインストールされてるコンピュータ
+    $dbname = getenv('heroku_02ff199312307ce'); //使用するDB
+    $charset = "utf8"; //文字コード
+    $user = getenv('bfc9b0befde9d2'); //MySQLにログインするユーザー名
+    $password = getenv('52614880'); //ユーザーのパスワード
 
     // POSTでデータが送信されたときのみSQLを実行する
     if (!empty($_POST)) {
