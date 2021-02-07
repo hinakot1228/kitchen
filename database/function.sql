d73c58ebfa4b4ae2e4c9f70231b23cbab3611aa3
Hinako-Tanaka <youngbird1228@gmail.com>
2021-02-07 16:03:23 +0900

    initial commit

---
+++ database/function.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2015 年 12 月 01 日 20:52
+-- サーバのバージョン： 5.6.26
+-- PHP Version: 5.5.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `todo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `booking` (
+  `id` int(11),
+  `name` varchar(225),
+  `date` date,
+  `time` time,
+  `people` varchar(255),
+  `email` text
+  `request` text
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `tasks`
+--
+
+INSERT INTO `booking` (`id`, `name`, `date`, `time`, `people`, `email`, `request`) VALUES
+(1, 'テスト', '2016-01-01', '00:00:00', '1-2', 'sample.gmail.com', 'こんにちは');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `booking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
