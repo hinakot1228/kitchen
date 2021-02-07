b4ddf6b826c859af92f72154d36cb85fb6b12f5d
Hinako-Tanaka <youngbird1228@gmail.com>
2021-02-07 19:19:33 +0900

    fourth commit

--- thank.php
+++ thank.php
@@ -7,11 +7,17 @@ $email = htmlspecialchars($_POST['email']);
 $request = htmlspecialchars($_POST['request']);
 
 // 1. DBに接続
-$dsn = 'mysql:dbname=koreanrestaurant;host=localhost'; 
-$user = 'root'; 
-$password=''; 
-$dbh = new PDO($dsn, $user, $password); 
-$dbh->query('SET NAMES utf8'); 
+// $dsn = 'mysql:dbname=koreanrestaurant;host=localhost'; 
+// $user = 'root'; 
+// $password=''; 
+// $dbh = new PDO($dsn, $user, $password); 
+// $dbh->query('SET NAMES utf8'); 
+
+$host = getenv('us-cdbr-east-03.cleardb.com'); //MySQLがインストールされてるコンピュータ
+$dbname = getenv('heroku_02ff199312307ce'); //使用するDB
+$charset = "utf8"; //文字コード
+$user = getenv('bfc9b0befde9d2'); //MySQLにログインするユーザー名
+$password = getenv('52614880'); //ユーザーのパスワード
 
 // 2. SQL文の実行
 $sql = 'INSERT INTO `booking`(`name`, `date`, `time`, `people`, `email`, `request`) VALUES ("'. $name.'", "'. $date.'", "'.$time.'", "' . $people.'", "' . $email. '", "' . $request. '")'; 
--- view.php
+++ view.php
@@ -1,10 +1,16 @@
 <?php 
 // 1. DBに接続
-$dsn = 'mysql:dbname=koreanrestaurant;host=localhost'; 
-$user = 'root'; 
-$password=''; 
-$dbh = new PDO($dsn, $user, $password); 
-$dbh->query('SET NAMES utf8'); 
+// $dsn = 'mysql:dbname=koreanrestaurant;host=localhost'; 
+// $user = 'root'; 
+// $password=''; 
+// $dbh = new PDO($dsn, $user, $password); 
+// $dbh->query('SET NAMES utf8'); 
+
+$host = getenv('us-cdbr-east-03.cleardb.com'); //MySQLがインストールされてるコンピュータ
+$dbname = getenv('heroku_02ff199312307ce'); //使用するDB
+$charset = "utf8"; //文字コード
+$user = getenv('bfc9b0befde9d2'); //MySQLにログインするユーザー名
+$password = getenv('52614880'); //ユーザーのパスワード
 
 // 2. SQL文を実行
 $sql = 'SELECT * FROM `booking`'; 
